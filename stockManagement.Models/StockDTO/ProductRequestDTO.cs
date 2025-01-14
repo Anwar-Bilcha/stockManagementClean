d71cb29fcc4f56c40ffc6cb1f3fea579ed9537e6
Anwar-Bilcha <anwarbilcha2019@gmail.com>
2025-01-14 22:14:35 +0300

    DTO Files for Product Created

---
+++ stockManagement.Models/StockDTO/ProductRequestDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stockManagement.Models.StockDTO
+{
+    public class ProductRequestDTO
+    {
+        public string ProductId { get; set; }
+
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public double Price { get; set; }
+
+        public double Quantity { get; set; }
+    }
+}
---
+++ stockManagement.Models/StockDTO/ProductResponseDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stockManagement.Models.StockDTO
+{
+    public class ProductResponseDTO
+    {
+        public string ProductId { get; set; }
+
+        public string ProductName { get; set; }
+        public string ProductCategory { get; set; }
+        public double Price { get; set; }
+
+    }
+}
