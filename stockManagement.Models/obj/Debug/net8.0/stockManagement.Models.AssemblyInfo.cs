1ad2e70971692d474c1ed4d4080e09b82811d0d8
Anwar-Bilcha <anwarbilcha2019@gmail.com>
2025-01-16 21:48:01 +0300

    nullDone

--- stockManagement.Models/obj/Debug/net8.0/stockManagement.Models.AssemblyInfo.cs
+++ stockManagement.Models/obj/Debug/net8.0/stockManagement.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("stockManagement.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d81b2b4b24c8577d6ace935024db93f130ca5203")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39c2bccb249d0b7d4f762a627efa2baee3ef64ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("stockManagement.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("stockManagement.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
